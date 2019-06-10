0c3a0e790bc646082c7f832ed24f66b65d23a9e0
pratikdhody <hello@tecks.com.au>
2019-06-10 20:44:21 +1000

    bkup

--- services/passport.js
+++ services/passport.js
@@ -22,18 +22,15 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback',
     proxy: true
 }, 
-(accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id})
-    .then((existingUser) => {
-        if (existingUser) {
-            // we already have a record with the given profile ID
-            done(null, existingUser);
-        } else {
-            // we don't have a user record so let's create it.
-            new User({ googleId: profile.id, displayName: profile.displayName})
-            .save()
-            .then(user => done(null, user));
-        }
+async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id})
+    if (existingUser) {
+        // we already have a record with the given profile ID
+        done(null, existingUser);
+    } else {
+        // we don't have a user record so let's create it.
+        const user = await new User({ googleId: profile.id, displayName: profile.displayName}).save()
+        done(null, user);
+    }
     })
-    })
-);
+    );
