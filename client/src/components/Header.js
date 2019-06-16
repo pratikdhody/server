3e89fa83fd13ebf52d1012a7a4cdfe5507539967
pratikdhody <hello@tecks.com.au>
2019-06-16 21:20:07 +1000

    20190616

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,13 @@ class Header extends Component {
             default: 
                 return [
                     <li key="1"><Payments /></li>,
-                    <li key="2"><a href="/api/logout">Log out</a></li>
+                    <li key="4" style={{margin: '0 10px'}}>
+                        User: {this.props.auth.displayName}
+                    </li>,
+                    <li key="2" style={{margin: '0 10px'}}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="3"><a href="/api/logout">Log out</a></li>
                 ];
         }
     }
