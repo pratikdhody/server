6dd2e9e8ea7f0fca3ed7fdde6de9097de277374c
pratikdhody <hello@tecks.com.au>
2019-09-17 22:10:52 +1000

    20190917

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,34 +1,47 @@
 // SurveyFormReview shows value for review
-import _ from 'lodash';
-import React from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
+import _ from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import * as actions from "../../actions";
+import { withRouter } from "react-router-dom";
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
-    const reviewFields = _.map(formFields, ({name, label}) => {
-        return (
-            <div key={name}>
-                <label>{label}</label>
-                <div>
-                    {formValues[name]}
-                </div>
-            </div>
-        );
-    });
-
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
-        <div>
-            <h5>Please confirm your entries</h5>
-            {reviewFields}
-            <button className="yellow darken-3 btn-flat" onClick={onCancel}>
-                Back
-            </button>
-        </div>
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
     );
+  });
+
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      {reviewFields}
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
 };
 
 function mapStateToProps(state) {
-    return { formValues: state.form.surveyForm.values };
-    }
+  return { formValues: state.form.surveyForm.values };
+}
 
-export default connect(mapStateToProps) (SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(SurveyFormReview));
